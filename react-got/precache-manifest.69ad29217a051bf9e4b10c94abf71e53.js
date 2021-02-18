5da89e431923f20730c9429479e44da6008030c6
Sultanahmet Kazbek <64279898+sult3003@users.noreply.github.com>
2021-02-18 13:48:30 +0600

    Add files via upload

---
+++ react-got/precache-manifest.69ad29217a051bf9e4b10c94abf71e53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "47a9016bddc6bbf2827f",
+    "url": "/static/css/main.81de1790.chunk.css"
+  },
+  {
+    "revision": "47a9016bddc6bbf2827f",
+    "url": "/static/js/main.47a9016b.chunk.js"
+  },
+  {
+    "revision": "11fdb1d2868f35f103dc",
+    "url": "/static/css/1.9aa3da59.chunk.css"
+  },
+  {
+    "revision": "11fdb1d2868f35f103dc",
+    "url": "/static/js/1.11fdb1d2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eb8d5afb52ce859c7cfe920af2a73269",
+    "url": "/index.html"
+  }
+];
